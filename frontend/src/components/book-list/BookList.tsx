cb4d3db46e7f07ff56c2d43679efc71b8021cb59
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-31 16:19:25 +0300

    Delete book by id

--- frontend/src/components/book-list/BookList.tsx
+++ frontend/src/components/book-list/BookList.tsx
@@ -1,7 +1,14 @@
-import { useSelector } from 'react-redux'
+import { FaTrashCan } from 'react-icons/fa6'
+import { useDispatch, useSelector } from 'react-redux'
+import { deleteBook } from '../../redux/books/actionCreators'
 import './BookList.css'
+
 const BookList = () => {
+	const dispatch = useDispatch()
 	const books = useSelector(state => state.books)
+	const handleDelete = id => {
+		dispatch(deleteBook(id))
+	}
 	return (
 		<div className='app-block book-list'>
 			<h2>BookList</h2>
@@ -12,7 +19,13 @@ const BookList = () => {
 					{books.map((book, i) => (
 						<li key={book.id}>
 							<div className='book-info'>
-							{++i}.	{book.title} by <strong>{book.author}</strong>
+								{++i}. {book.title} by <strong>{book.author}</strong>
+							</div>
+							<div
+								onClick={() => handleDelete(book.id)}
+								className='book-actions'
+							>
+								<FaTrashCan />
 							</div>
 						</li>
 					))}
