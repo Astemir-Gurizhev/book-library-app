1b3aec3cb380f299b9cdac3c766b1277bfc596e7
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-31 12:32:10 +0300

    Creating components and adding styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
 import './App.css'
+import BookForm from './components/book-form/BookForm'
+import BookList from './components/book-list/BookList'
+import Filter from './components/filter/Filter'
 
 function App() {
-	return <div className='app'></div>
+	return (
+		<div className='app'>
+			<header className='app-header'>
+				<h1>Book Library App</h1>
+			</header>
+			<main className='app-main'>
+				<div className='app-left-column'>
+					<BookForm />
+				</div>
+				<div className='app-right-column'>
+					<Filter />
+					<BookList />
+				</div>
+			</main>
+		</div>
+	)
 }
 
 export default App
---
+++ frontend/src/components/book-form/BookForm.tsx
@@ -0,0 +1,10 @@
+import './BookForm.css'
+const BookForm = () => {
+	return (
+		<div className='app-block book-form'>
+			<h2>BookForm</h2>
+		</div>
+	)
+}
+
+export default BookForm
