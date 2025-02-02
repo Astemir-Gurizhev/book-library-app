98676ed3ae666ed4018b4e4cacb83d5729e8897a
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-02 19:45:04 +0300

    Sending an action to get the input status

--- frontend/src/components/filter/Filter.tsx
+++ frontend/src/components/filter/Filter.tsx
@@ -1,9 +1,20 @@
+import { useDispatch } from 'react-redux'
+import { setTitleFilter } from '../../redux/slices/filterSlice'
 import './Filter.css'
 const Filter = () => {
+	const dispatch = useDispatch()
+	const handleTitleFilterChange = e => {
+		dispatch(setTitleFilter(e.target.value))
+	}
+
 	return (
 		<div className='app-block filter'>
-			<div className="filter-group">
-				<input type="text" placeholder='Filter by title...'/>
+			<div className='filter-group'>
+				<input
+					onChange={handleTitleFilterChange}
+					type='text'
+					placeholder='Filter by title...'
+				/>
 			</div>
 		</div>
 	)
