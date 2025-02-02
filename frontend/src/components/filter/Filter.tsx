8dbab161afa29f2d9fad4cb2fd91ab9b2a1fac72
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-02 19:50:38 +0300

    Synchronization of the filter input field by book name with the status

--- frontend/src/components/filter/Filter.tsx
+++ frontend/src/components/filter/Filter.tsx
@@ -1,8 +1,9 @@
-import { useDispatch } from 'react-redux'
-import { setTitleFilter } from '../../redux/slices/filterSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectTitleFilter, setTitleFilter } from '../../redux/slices/filterSlice'
 import './Filter.css'
 const Filter = () => {
 	const dispatch = useDispatch()
+	const titleFilter = useSelector(selectTitleFilter)
 	const handleTitleFilterChange = e => {
 		dispatch(setTitleFilter(e.target.value))
 	}
@@ -11,6 +12,7 @@ const Filter = () => {
 		<div className='app-block filter'>
 			<div className='filter-group'>
 				<input
+					value={titleFilter}
 					onChange={handleTitleFilterChange}
 					type='text'
 					placeholder='Filter by title...'
